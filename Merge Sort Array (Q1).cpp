5076172d7f1cea2f0ff3cc98cf9d07c2ae28fd0a
Aishwarya Jha <aishjha20@gmail.com>
2023-06-24 19:16:50 +0530

    updated ques 1

--- Merge Sort Array (Q1).cpp
+++ Merge Sort Array (Q1).cpp
@@ -2,24 +2,32 @@
 using namespace std;
 
 void merge(vector<int>& arr, int l, int mid, int h) {
-    if(l >= h)
-        return;
-        
-    vector<int> left(arr.begin() + l, arr.begin() + mid);
-    vector<int> right(arr.begin() = mid + 1, arr.end());
+    vector<int> left(arr.begin() + l, arr.begin() + mid + 1);
+    vector<int> right(arr.begin() + mid + 1, arr.end());
 
     int i = 0, j = 0, k = l;
     while(i < left.size() && j < right.size()) {
-        if(left[i] < right[i]) {
+        if(left[i] < right[j]) {
             arr[k] = left[i];
             i++;
         }
-
         else {
             arr[k] = right[j];
             j++;
         }
         k++;
+    }
+
+    while(i < left.size()) {
+        arr[k] = left[i];
+        k++;
+        i++;
+    }
+
+    while(j < right.size()) {
+        arr[k] = right[j];
+        k++;
+        j++;
     }
 }
 
